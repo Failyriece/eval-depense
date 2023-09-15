329285caebf77a75fb676a529b24f52a617aad22
Failyriece <failyriece@gmail.com>
2023-09-15 13:21:52 +0200

    Dépenses (avec catégories, tous est moche)

--- src/components/AllDepense/index.jsx
+++ src/components/AllDepense/index.jsx
@@ -4,18 +4,23 @@ function AllDepense() {
     const { state } = useDepense();
 
     const test = state.reduce((acc, obj) => {return acc + obj.price}, 0);
+
+    const filtre = (filt) => {
+        return state.filter((depense) => depense.category == filt).reduce((acc, obj) => {return acc + obj.price}, 0);
+    }
     console.log(test)
     return (
         <>
             Dépenses total: {test}
+            Dépenses alimentaire: {filtre('Alimentation')}
+            Dépenses de logement: {filtre('Logement')}
+            Dépenses de tranport: {filtre('Transport')}
+            Dépenses divertissement: {filtre('Divertissement')}
+            Dépenses santé: {filtre('Santé')}
+            Dépenses éducative: {filtre('Éducation')}
+            Dépenses autres: {filtre('Autres')}
         </>
     )
 }
 
 export default AllDepense
-
-
-// const sumWithInitial = array1.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
-// const calcTotal = (state) => {
-//     return state.reduce((accumulator, ...price) => {
-//         return accumulator + state.totalPrice;}, 0);};
